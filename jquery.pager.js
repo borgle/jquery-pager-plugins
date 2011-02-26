3d5040a324e1cc7d3018929c80785c3eb17845b4
yoker.wu@gmail.com <yoker.wu@gmail.com>
2011-02-27 03:58:40 +0800

    增加演示文件以及压缩后的文件
    添加了具体的使用说明以及演示文件

--- jquery.pager.js
+++ jquery.pager.js
@@ -1,4 +1,37 @@
-﻿(function($) {
+﻿/*
+* jQuery pager plugin
+* Version 1.0 (02/27/2011)
+* @requires jQuery v1.2.6 or later
+*
+* Example at: https://github.com/yoker/jquery-pager-plugins/raw/master/demo.html
+*
+* Copyright (c) 2011 Yoker Wu
+* Dual licensed under the MIT and GPL licenses:
+* http://www.opensource.org/licenses/mit-license.php
+* http://www.gnu.org/licenses/gpl.html
+* 效果:  < [4] [5] [6] [7] [8] >
+* Usage: 
+ .pager({ callback: go });
+ .pager({listnum: 9,totalrecords: 58,pagesize: 5,pageindex: 6,callback:go});
+*
+* Where listnum is 分割数量
+*       pagesize is 每页条数
+        totalrecords is 总条数
+        pageindex is 当前页码;
+*       callback is the method to fire when a pager button is clicked.
+*
+* callback signiture is go = function(pageclickednumber) 
+* Where pageclickednumber is the number of the page clicked in the control.
+*
+* The css file is a dependancy but can obviously tweaked to your wishes
+    #pager a {display:block;text-transform:uppercase;font-size:12px;float:left;}
+    #pager a.number {border:1px solid #ccc;text-decoration:none;margin:0 5px 0 0;padding:5px;background:#f0f0f0;color:#333;}
+    #pager a.hover {border:1px solid #f00;}
+    #pager a.empty {display:none;}
+    #pager a.current {border:1px solid #003f7e;color:#000;font-weight:700;background-color:#eee;}
+*/
+
+(function($) {
 
     $.fn.pager = function(options) {
 
